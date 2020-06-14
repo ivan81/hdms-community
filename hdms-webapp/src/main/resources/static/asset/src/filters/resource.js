56ed2fcd102fc57e4a658217bf4355be5e12372a
ivanhuang <huangjianghua@x-ri.com>
2020-06-14 18:57:08 +0800

    修改pom描述和文件名

--- hdms-webapp/src/main/resources/static/asset/src/filters/resource.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-/* Filters */
-// need load the moment.js to use this filter.
-angular.module('hc.util').filter('resource', ['$rootScope',function($rootScope) {
-	return function(resource, type) {
-		return resource;
-	}
-}]);
